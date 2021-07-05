9498f4a9d3e996db49f494a31e28476c8e2b4c61
doganakt <84280698+doganakt@users.noreply.github.com>
2021-07-06 01:26:02 +0300

    Update Crypto_World_Project.py

--- Crypto_World_Project.py
+++ Crypto_World_Project.py
@@ -1,13 +1,22 @@
 #CryptoWorld Project
 import numpy as np
-import pandas as np
+import pandas as pd
+
 
 #Some given parameters. In the next time, it is tried to retrieved the most updated data.
 
-BTCamount=0
-ETHamount=0
-BTCValue=5
-ETHValue=2
+Price_Coin = pd.read_excel (r'C:\Users\Doğan\Desktop\Doğan\Crypto_World\CoinPrices.xlsx')
+BTCamount=0.00000000
+ETHamount=0.00000000
+LTCamount=0.00000000
+BCHamount=0.00000000
+XRPamount=0.00000000
+
+BTCValue= float(Price_Coin['BTCUSD'])
+ETHValue= float(Price_Coin['ETHUSD'])
+LTCValue= float(Price_Coin['LTCUSD'])
+BCHValue= float(Price_Coin['BCHUSD'])
+XRPValue= float(Price_Coin['XRPUSD'])
 
 #Create the wallet
 
@@ -20,6 +29,10 @@ if a==1:
     
     BTCamount=float(input('How many BTC do you have?:'))
     ETHamount=float(input('How many ETH do you have?:'))
+    LTCamount=float(input('How many LTC do you have?:'))
+    BCHamount=float(input('How many BCH do you have?:'))
+    XRPamount=float(input('How many XRP do you have?:'))
+    
     print("\nSummary of your wallet is as follows:")
     MyWallet.info()
 
@@ -39,6 +52,23 @@ if b==2:
 else:
     k=1
     while k>0:
-        print("Here is the list of data you can see\nPlease type related number to see corresponding data:")
-        deneme = Historical_Info('BTC')
-        k=0
+        Parameter = int(input("Here is the list of parameters you can examine\n1-Historical Information, Graphics and Some Statistics\n2-Trend Analysis\n3-RSI Analysis\n4-MACD Analysis\nPlease type related number to see corresponding data:"))
+        if Parameter == 1:
+            coin = input("Enter the coin you would like to investigate: ")
+            history = Historical_Info(coin)
+        elif Parameter == 2:
+            coin = input("Enter the coin you would like to investigate: ")
+            trend = Trend_Analysis(coin)
+        elif Parameter == 3:
+            print("RSI ekle")
+        else:
+            print("MACD Analysis will be loaded to the program soon!")
+        k=int(input("If you would like to investigate another coin or parameter, please type '1'; otherwise, type '0' to update your wallet!:"))
+ 
+ 
+#Ask for buying / selling cryptocurrencies and update the wallet
+
+
+
+
+ 
